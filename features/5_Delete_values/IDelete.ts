2408592b8488e0fd866bf9925d9525fda2f02589
Gabriel Lopes <gabrielrrlopes@gmail.com>
2023-03-21 21:34:43 -0400

    Fixed rebase mistake.

--- features/5_Delete_values/IDelete.ts
+++ features/5_Delete_values/IDelete.ts
@@ -1,13 +1,7 @@
 interface Delete {
-<<<<<<< HEAD
   user: string;
   activity: string;
   workdate: string;
-=======
-  User: string;
-  Activity: string;
-  Workday: string;
->>>>>>> 6b770a3... Added table handler.
 }
 
 export { Delete };
